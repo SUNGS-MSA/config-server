76898fadfa914f8d283f0ce7e0064430862964d9
Administrator <Administrator@DESKTOP-2RU7GA4>
2024-06-05 23:19:00 +0900

    gggg

--- src/main/java/sungs/msa/ConfigServerApplication.java
+++ src/main/java/sungs/msa/ConfigServerApplication.java
@@ -2,10 +2,12 @@ package sungs.msa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient  //유레카 클라이언트 적용
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
